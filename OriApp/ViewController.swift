3878166aaf626f0deea520fec54e3cb0e190f1b9
今井咲江 <imaisakie@imaisakikounomacbook-ea.local>
2018-08-04 01:26:01 +0900

    Initial Commit

---
+++ OriApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OriApp
+//
+//  Created by 今井咲江 on 2018/08/04.
+//  Copyright © 2018年 imai sakie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
