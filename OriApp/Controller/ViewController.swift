388399b1a69036453daa4ebf090c0404497ddfa6
今井咲江 <sgttmkz05@gmail.com>
2018-12-07 17:56:30 +0900

    ダイスロール画面のチェックボタンとラベルの設定と配置

--- OriApp/Controller/ViewController.swift
+++ OriApp/Controller/ViewController.swift
@@ -97,7 +97,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
     /*--------------ダメボ--------------*/
     @IBOutlet weak var DBnLabel: UILabel!
     
+    /*--------------checkboxButton--------------*/
+    var ALLCheckbox: UIButton!
+    var STRCheckbox: UIButton!
+    var CONCheckbox: UIButton!
+    var POWCheckbox: UIButton!
+    var DEXCheckbox: UIButton!
+    var APPCheckbox: UIButton!
+    var SIZCheckbox: UIButton!
+    var INTCheckbox: UIButton!
+    var EDUCheckbox: UIButton!
     
+    var checkboxArray: [UIButton] = []
+    
+    var roleSelectView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -105,6 +118,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         STRpmTextField.delegate = self
         STRtTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -118,7 +132,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let backgroundView = makeBackgroundView()
         self.view.addSubview(backgroundView)
         
-        let roleSelectView = makeRoleSelectView()
+        roleSelectView = makeRoleSelectView()
         backgroundView.addSubview(roleSelectView)
         
         let titleLabel = makeTileLabel()
@@ -128,12 +142,127 @@ class ViewController: UIViewController, UITextFieldDelegate {
         roleSelectView.addSubview(lineView)
         
         //ダイスロールボタン
-        let roleButton = UIButton()
+        let roleButton = makeRoleButton()
         roleSelectView.addSubview(roleButton)
+        
+        //チェックボックスの作成
+        makeAllCheckbox()
+        
+        //ラベルの作成
+        makeAllLabel()
         
     }
     
-    //部品生成のための処理
+    
+    /*--------------チェックボックスの処理--------------*/
+    @objc func onClickMyButton(sender: UIButton) {
+        //ボタンが押されたら現在のmyButtonのBool値の反対の値が入る(チェックされたらtrue, 外れたらfalseが代入される)
+        sender.isSelected = !sender.isSelected
+//        print(sender.isSelected)
+        
+        if (sender == ALLCheckbox) && (ALLCheckbox.isSelected == true) {
+            for box in checkboxArray {
+                box.isSelected = true
+            }
+        } else if (sender == ALLCheckbox) && (ALLCheckbox.isSelected == false) {
+            for box in checkboxArray {
+                box.isSelected = false
+            }
+        }
+    }
+    
+    
+    /*--------------部品生成のための処理--------------*/
+    func makeAllCheckbox() {
+        //チェックボックス
+        STRCheckbox = makeCheckbox(x: 5, y: 50, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(STRCheckbox)
+        checkboxArray.append(STRCheckbox)
+        
+        CONCheckbox = makeCheckbox(x: 100, y: 50, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(CONCheckbox)
+        checkboxArray.append(CONCheckbox)
+        
+        POWCheckbox = makeCheckbox(x: 195, y: 50, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(POWCheckbox)
+        checkboxArray.append(POWCheckbox)
+        
+        DEXCheckbox = makeCheckbox(x: 5, y: 125, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(DEXCheckbox)
+        checkboxArray.append(DEXCheckbox)
+        
+        APPCheckbox = makeCheckbox(x: 100, y: 125, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(APPCheckbox)
+        checkboxArray.append(APPCheckbox)
+        
+        SIZCheckbox = makeCheckbox(x: 195, y: 125, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(SIZCheckbox)
+        checkboxArray.append(SIZCheckbox)
+        
+        INTCheckbox = makeCheckbox(x: 5, y: 200, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(INTCheckbox)
+        checkboxArray.append(INTCheckbox)
+        
+        EDUCheckbox = makeCheckbox(x: 100 , y: 200, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(EDUCheckbox)
+        checkboxArray.append(EDUCheckbox)
+        
+        ALLCheckbox = makeCheckbox(x: 195, y: 200, width: 40, height: 40, action: #selector(self.onClickMyButton(sender:)))
+        roleSelectView.addSubview(ALLCheckbox)
+        checkboxArray.append(ALLCheckbox)
+    }
+    
+    
+    func makeCheckbox(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, action: Selector) -> UIButton {
+        let myButton = UIButton()
+        myButton.frame = CGRect(x: x, y: y, width: width, height: height)
+        myButton.addTarget(self, action: action, for: .touchUpInside)
+        myButton.setImage(UIImage(named: "checkbox.png"), for: .selected)
+        myButton.setImage(UIImage(named: "nocheckbox.png"), for: .normal)
+        return myButton
+    }
+    
+    
+    func makeAllLabel() {
+        //チェックボックスのラベル
+        let STRLabel = makeCheckboxLabel(x: 47, y: 50, width: 40, height: 40, title: "STR")
+        roleSelectView.addSubview(STRLabel)
+        
+        let CONLabel = makeCheckboxLabel(x: 142, y: 50, width: 40, height: 40, title: "CON")
+        roleSelectView.addSubview(CONLabel)
+        
+        let POWLabel = makeCheckboxLabel(x: 237, y: 50, width: 40, height: 40, title: "POW")
+        roleSelectView.addSubview(POWLabel)
+        
+        let DEXLabel = makeCheckboxLabel(x: 47, y: 125, width: 40, height: 40, title: "DEX")
+        roleSelectView.addSubview(DEXLabel)
+        
+        let APPLabel = makeCheckboxLabel(x: 142, y: 125, width: 40, height: 40, title: "APP")
+        roleSelectView.addSubview(APPLabel)
+        
+        let SIZLabel = makeCheckboxLabel(x: 237, y: 125, width: 40, height: 40, title: "SIZ")
+        roleSelectView.addSubview(SIZLabel)
+        
+        let INTLabel = makeCheckboxLabel(x: 47, y: 200, width: 40, height: 40, title: "INT")
+        roleSelectView.addSubview(INTLabel)
+        
+        let EDULabel = makeCheckboxLabel(x: 142, y: 200, width: 40, height: 40, title: "EDU")
+        roleSelectView.addSubview(EDULabel)
+        
+        let AllLabel = makeCheckboxLabel(x: 237, y: 200, width: 40, height: 40, title: "ALL")
+        roleSelectView.addSubview(AllLabel)
+    }
+    
+    
+    func makeCheckboxLabel(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, title: String) -> UILabel {
+        let myLabel = UILabel()
+        myLabel.frame = CGRect(x: x, y: y, width: width, height: height)
+        myLabel.text = title
+        myLabel.textAlignment = NSTextAlignment.left
+        myLabel.font = UIFont(name: "HiraKakuProN-W6", size: 15)
+        return myLabel
+    }
+    
     func makeRoleButton() -> UIButton {
         let roleButton = UIButton()
         roleButton.frame = CGRect(x: 50, y: 260, width: 200, height: 20)
@@ -163,7 +292,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return roleSelectView
     }
     
-    //装飾部品の生成のための処理
+    /*--------------装飾部品の生成のための処理--------------*/
     func makeTileLabel() -> UILabel {
         let titleLabel = UILabel()
         titleLabel.frame = CGRect(x: 10, y: 5, width: 280, height: 40)
